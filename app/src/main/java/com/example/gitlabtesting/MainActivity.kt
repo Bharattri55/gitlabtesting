7ccf067084813683ee56474a842ce37f3cb4c6e4
mohits10 <mohits10@chetu.com>
2022-02-19 18:23:40 +0530

    "Remove commented line"

--- app/src/main/java/com/example/gitlabtesting/MainActivity.kt
+++ app/src/main/java/com/example/gitlabtesting/MainActivity.kt
@@ -8,17 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
-        // Mogitg db swdnd  asdfbdfjkADFNKQe
     }
 }
